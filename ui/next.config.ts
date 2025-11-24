c73cc187c73d263f593e95b1842543eb18f9b1b3
QuynhThu17 <thuquynh17022004bang@gmail.com>
2025-11-24 10:20:34 +0700

    Update configuration and package-lock for image handling and peer dependencies

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // distDir: ".next-dev-alt",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mekongai-social.s3.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.us-east-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
